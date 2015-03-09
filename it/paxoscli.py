a354f77f5d32db10d6b064f84bf2017019144d20
drdr xp <drdr.xp@gmail.com>
2015-03-09 14:22:26 +0800

    paxoscli use paxosclient.py

--- it/paxoscli.py
+++ it/paxoscli.py
@@ -3,69 +3,23 @@ import json
 import urllib
 
 import it.http
+import it.paxosclient
 
-class PaxosError( Exception ):
-    def __init__( self, *args, **argkv ):
-        self.Code = argkv.get( 'Code' )
-        self.Message = argkv.get( 'Message' )
+PaxosError = it.paxosclient.PaxosError
 
-class PaxosClient( object ):
+class PaxosClient( it.paxosclient.PaxosClient ):
 
     api_uri_prefix = '/api'
 
     def __init__( self, ident ):
-        self.ident = int( ident )
-        self.cluster_id = 'x'
+        ip, port, cluster_id = ip_port_cid(ident)
+        super(PaxosClient, self).__init__(ip, port, cluster_id, ident)
 
-    def send_cmd( self, cmd, reqbody=None ):
-
-        req = { 'cmd':cmd }
-
-        if reqbody is not None:
-            req.update( reqbody )
-
-        uri = self.make_uri( req )
-
-        if reqbody is not None:
-            reqbody = json.dumps(reqbody)
-
-        return self.http( uri, req_body=reqbody )
-
-    def http( self, uri, req_body=None ):
-
-        req_body = req_body or ''
-
-        h = it.http.Http( '127.0.0.1', 9080+int(self.ident), timeout=3 )
-        h.send_request( uri, headers={ 'Content-Length': len( req_body ) } )
-        h.send_body( req_body )
-        h.finish_request()
-
-        body = h.read_body( 1024*1024 )
-        try:
-            body = json.loads( body )
-        except:
-            pass
-        return { 'status': h.status,
-                 'headers': h.headers,
-                 'body': body, }
-
-    def make_uri( self, req ):
-
-        uri = self.api_uri_prefix \
-                + ('/{cluster_id}/{ident}/{cmd}'.format(
-                        cluster_id=self.cluster_id,
-                        ident=self.ident,
-                        cmd=req[ 'cmd' ],
-                ))
-
-        query_keys = [ 'ver' ]
-        q = {}
-        for k in query_keys:
-            if k in req:
-                q[ k ] = req[ k ]
-
-        uri += '?' + urllib.urlencode( q )
-        return uri
+def ip_port_cid(ident):
+    ip = '127.0.0.1'
+    port = 9080+int(ident)
+    cluster_id = 'x'
+    return ip, port, cluster_id
 
 def ids_dic(ids):
     return dict([ (str(x),str(x))
@@ -78,9 +32,11 @@ def init_view( ident, view_ids, ver=1 ):
 
     view = [ ids_dic(x) for x in view_ids ]
 
-    return request( 'phase3', ident, { 'ver':ver, 'val': {
-            'view': view,
-    } } )
+    return request( 'phase3', ident,
+                    { 'ver':ver,
+                      'val': {
+                              'view': view,
+                      } } )
 
 def request( cmd, ident, body=None ):
     if cmd == 'get_leader':
@@ -89,16 +45,9 @@ def request( cmd, ident, body=None ):
     elif cmd == 'get_view':
         cmd, body = 'get', {"key":"view"}
 
-    p = PaxosClient( ident )
-    rst = p.send_cmd(cmd, reqbody=body)
-    return rst
+    ip, port, cluster_id = ip_port_cid(ident)
+    return it.paxosclient.request(ip, port, cluster_id, ident, cmd, body=body)
 
-def req_ex( cmd, to_ident, body=None ):
-
-    rst = request( cmd, to_ident, body )
-    b = rst[ 'body' ]
-    if 'err' in b:
-        e = b[ 'err' ]
-        raise PaxosError( **e )
-
-    return b
+def request_ex( cmd, to_ident, body=None ):
+    ip, port, cluster_id = ip_port_cid(to_ident)
+    return it.paxosclient.request_ex(ip, port, cluster_id, to_ident, cmd, body=body)
